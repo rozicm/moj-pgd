bdad6accdd18724e4b18a0f607d05cce9f532ff4
Matic_Rozic <rozicmatic@gmail.com>
2024-02-05 23:28:50 +0100

    fix

--- src/components/AuthShowcase.tsx
+++ src/components/AuthShowcase.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
-// import { api } from "~/utils/api";
+import { api } from "~/utils/api";
 
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  // const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-  //   undefined,
-  //   { enabled: sessionData?.user !== undefined },
-  // );
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined,
+    { enabled: sessionData?.user !== undefined },
+  );
 
   return (
     <div className="flex items-center justify-center pr-2 text-xl">
-      {/* <p className="color-white text-center text-2xl">
+      <p className="color-white text-center text-2xl">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
-      </p> */}
+      </p>
       <button
         className={`transform rounded-full px-8 py-2 font-semibold no-underline transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 ${
           sessionData
