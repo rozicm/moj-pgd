1a4341a9a8636cd9b0df9a739da90c128105e9d4
Matic_Rozic <rozicmatic@gmail.com>
2024-04-17 20:41:35 +0200

    neki

--- next.config.mjs
+++ next.config.mjs
@@ -19,4 +19,12 @@ const config = {
   },
 };
 
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
 export default config;
